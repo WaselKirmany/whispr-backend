e98125d1cbe6b204965fe41a1d1cb846425f42d3
Wasel Kirmany <waselkirmany25@gmail.com>
2025-07-08 22:00:24 +0530

    frontend url

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use((req,res,next) => {
 });
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL, // Replace with your frontend URL
     credentials: true, // Allow credentials (cookies, authorization headers, etc.)
 }));
 app.use(cookieParser());
