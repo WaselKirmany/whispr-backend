2b329a8baa47523f53dfcb5eff76e06d14bbba7a
Wasel Kirmany <waselkirmany25@gmail.com>
2025-07-09 14:51:59 +0530

    cors

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "https://whispr-react.vercel.app",
+  origin: process.env.FRONTEND_URL,
   credentials: true
 }));
 app.options('*', cors()); // To handle preflight requests
