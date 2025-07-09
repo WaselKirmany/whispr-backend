6e704ec1c7b1f24cc924f750336f9f73798d1871
Wasel Kirmany <waselkirmany25@gmail.com>
2025-07-09 13:56:08 +0530

    reverting back..

--- index.js
+++ index.js
@@ -15,33 +15,10 @@ import authRoutes from './routes/auth.js';
 const app = express();
 
 // Middleware
-// app.use((req,res,next) => {
-//     res.header("Access-Control-Allow-Credentials", true);
-//     next();
-// });
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', 'https://whispr-react.vercel.app');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Add allowed methods
-//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Add allowed headers
-//   next();
-// });
-
-const allowedOrigins = [
-  'https://whispr-react.vercel.app',
-  'https://whispr-react-rb8w3tx74-wasel-kirmanys-projects.vercel.app', // preview URL
-];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('CORS not allowed from this origin'));
-    }
-  },
-  credentials: true
-}));
-
+app.use((req,res,next) => {
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
+});
 app.use(express.json());
 app.use(cors({
     origin: process.env.FRONTEND_URL, // Replace with your frontend URL
