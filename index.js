96b77f28fb6bb70524b913b7d6482650b1e0351b
Wasel Kirmany <waselkirmany25@gmail.com>
2025-07-09 15:02:37 +0530

    index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import dotenv from "dotenv";
-dotenv.config();
 
 import userRoutes from './routes/users.js';
 import postRoutes from './routes/posts.js';
@@ -10,28 +11,45 @@ import commentRoutes from './routes/comments.js';
 import likeRoutes from './routes/likes.js';
 import authRoutes from './routes/auth.js';
 
-const app = express();
 
+const app = express();
 app.use(express.json());
+// Middleware
+app.use((req,res,next) => {
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
+});
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  credentials: true
+    origin: process.env.FRONTEND_URL, // Replace with your frontend URL
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
 }));
+
+
+
+// app.use(cors({
+//   origin: process.env.FRONTEND_URL,
+//   credentials: true
+// }));
+// app.options('*', cors()); // To handle preflight requests
+
+
+
 app.use(cookieParser());
 
 // Routes
 app.use("/api/posts", postRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/comments", commentRoutes);
-app.use("/api/likes", likeRoutes);
-app.use("/api/auth", authRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/comments', commentRoutes);
+app.use('/api/likes', likeRoutes);
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the Social Media API');
+    res.send('Welcome to the Social Media API');
 });
 
-// app.listen(process.env.PORT || 3000, () => {
-//   console.log(`Server is running on port ${process.env.PORT || 3000}`);
-// });
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
 
 export default app;
