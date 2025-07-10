9ab796320d8922f5fcc405a61d9962109cdc0179
Wasel Kirmany <waselkirmany25@gmail.com>
2025-07-10 14:46:11 +0530

    verifyToken

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import {getPosts,createPost, getSinglePost,getPostsByUsername, getTopLikedPosts } from '../controllers/post.js';
+import { verifyToken } from "../middlewares/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/",getPosts);
-router.post("/", createPost); 
+router.get("/",verifyToken,getPosts);
+router.post("/",verifyToken, createPost); 
 router.get("/top-liked", getTopLikedPosts);
 router.get("/:id", getSinglePost);
 router.get("/user/:username", getPostsByUsername);
