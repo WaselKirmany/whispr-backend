00585d576194c5a43a49bb580a04f23de8bb618b
Wasel Kirmany <waselkirmany25@gmail.com>
2025-07-10 20:13:57 +0530

    comment.js

--- controllers/comment.js
+++ controllers/comment.js
@@ -2,15 +2,16 @@ import jwt from "jsonwebtoken";
 import { db } from "../connect.js";
 
 export const getComments = (req, res) => {
+  console.log("🔵 /api/comments/add route hit");
   const postId = req.params.postId;
 
   if (!postId) return res.status(400).json("Post ID is required");
 
   const q = `
-    SELECT c.*, u.username, u.profilePic
+    SELECT c.*, u.username, u.profilepic
     FROM comments c
-    JOIN users u ON c.userId = u.id
-    WHERE c.postId = $1
+    JOIN users u ON c.userid = u.id
+    WHERE c.postid = $1
     ORDER BY c.created_at DESC
   `;
 
@@ -25,13 +26,14 @@ export const getComments = (req, res) => {
 };
 
 export const addComment = (req, res) => {
+  console.log("🔵 /api/comments/ route hit");
   const token = req.cookies.accessToken;
   if (!token) return res.status(401).json("Not authenticated");
 
   jwt.verify(token, "secretkey", (err, userInfo) => {
     if (err) return res.status(403).json("Token invalid");
 
-    const q = "INSERT INTO comments (description, userId, postId) VALUES ($1, $2, $3)";
+    const q = "INSERT INTO comments (description, userid, postid) VALUES ($1, $2, $3)";
     // const values = [req.body.description, userInfo.id, req.body.postId];
     const values = [req.body.comment, userInfo.id, req.body.postId];
     db.query(q, values, (err, data) => {
